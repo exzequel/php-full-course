8613a9b13a1d82710f4d1f457fcec57cfdcd65fa
Exequel <exql.adzn@gmail.com>
2023-04-13 16:26:03 +0800

    initialize php intro

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    echo "I love pizza <br>";
+    echo "It's my favorite";
+    // This is a comment
+    /*
+    This is
+    a multi-line
+    comment
+    */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>Order Pizza</button>
+</body>
+</html>
