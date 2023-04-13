33aa2e9e3f1617eae6a1b674fd1e1f8f2c678b8b
Exequel <exql.adzn@gmail.com>
2023-04-13 16:56:44 +0800

    add arithemtic operators lecture code

---
+++ 03_lecture.php
@@ -0,0 +1,48 @@
+<?php
+    // Arithmetic Operators
+    // + - * / ** %
+
+    // Increment/Decrement Operators
+    // ++ --
+
+    // Operator Precedence
+    // ()
+    // **
+    // * / %
+    // + -
+
+    // Arithmetic Operators
+    $x = 10;
+    $y = 2;
+    $z = null;
+
+    // $z = $x + $y; // addition
+    // $z = $x - $y; // subtraction
+    // $z = $x * $y; // multiplication
+    // $z = $x / $y; // division
+    // $z = $x ** $y; // exponent (to the power of)
+    // $z = $x % $y; // modulus (remainder) - determining if a number is even or odd
+    echo $z;
+
+    // Increment/Decrement Operators
+    // $counter = 10;
+
+    // $counter = $counter + 1;
+    // $counter++;
+    // $counter--;
+
+    // $counter += 2; // increment by a set value
+    // $counter -= 3; // decrement by a set value
+    // echo $counter;
+
+    // Operator Precedence
+    // ()
+    // **
+    // * / %
+    // + -
+    
+    // $total = 1 + 2 - 3 * 4 / 5 ** 6;
+    // $total = 3 - 0.000768;
+    echo $total;
+
+?>
