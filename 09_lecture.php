03049329b5e6d6b63b2854ee3edb0c1a86aa38b1
Exequel <exql.adzn@gmail.com>
2023-04-16 13:57:17 +0800

    add 09_lecture for loop

---
+++ 09_lecture.php
@@ -0,0 +1,16 @@
+<?php
+// for loop - repeat some code a certain number of times
+
+    // for ($i = 1; $i <= 100; $i++) {
+    //     echo "$i <br>";
+    // }
+
+    // for($i = 2; $i <= 100; $i += 2) {
+    //     echo "$i <br>";
+    // }
+
+    for ($i = 10; $i > 0; $i--) {
+        echo "$i <br>";
+    }
+
+?>
