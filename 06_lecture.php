742c5b77d6fd70e160616466a3c05a9ee73d5079
Exequel <exql.adzn@gmail.com>
2023-04-16 10:18:12 +0800

    add 06_lecture if statements

---
+++ 06_lecture.php
@@ -0,0 +1,31 @@
+<?php
+// if statement - if condition is true, do something
+//                if condition is false, do nothing
+// order of if statements matters
+// pay attention to the order of your conditions
+
+// $age = 101;
+// if ($age >= 100) {
+//     echo "You are too old to enter this site";
+// }
+// else if ($age >= 18) {
+//     echo "You may enter this site";
+// }
+// elseif ($age <= 0) {
+//     echo "That wasn't a valid age, please try again";
+// } 
+
+// else {
+//     echo "You must be at least 18 years old to enter this site";
+// }
+
+// $adult = true;
+
+// if ($adult) {
+//     echo "You may enter this site";
+// }
+// else {
+//     echo "You must be an adult to enter";
+// }
+
+?>
